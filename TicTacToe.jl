6b900477c30e26f8a2c6ec68d0d96a6db6d02247
Emanuel Geromin <egeromin@gmail.com>
2018-04-20 17:45:03 -0400

    Got accuracy up to 0.97 for slightly clever player

--- TicTacToe.jl
+++ TicTacToe.jl
@@ -2,13 +2,21 @@ module TicTacToe
 
 using Random
 
-function allowed_next_states(current_state)
+
+@enum Player nobody me opponent
+BOARD_SIZE = 9
+
+
+struct Board
+    cells::Array{Player, 1}
+
+    Board(cells) = new(copy(cells))  # copy by default on constructing
+    Board() = new(repeat([nobody], BOARD_SIZE))
 end
 
 
-export display_state
-function display_state(state_num)
-    [parse(Int64, x) for x in string(state_num-1, base=3, pad=9)]
+function board_from_index(i::Int64)
+    Board([Player(parse(Int64, x)) for x in string(i-1, base=3, pad=9)])
 end
 
 
@@ -23,34 +31,42 @@ function get_lines()
 end
 
 
-export get_winner
-function get_winner(state)
-    lines = get_lines()
+LINES = get_lines()
 
-    function single_color(line_ind)
-        line = lines[line_ind,:]
-        if (state[line[1]] == state[line[2]]) && (state[line[2]] == state[line[3]])
-            return state[line[1]]
+
+function get_winner(board::Board)
+
+    for i in 1:size(LINES, 1)
+        line = LINES[i, :]
+        a, b, c = map(j -> board.cells[j], line)
+        if a == b == c
+            return a
         end
-        return 0
     end
 
-    winners = Set(single_color(line_ind) for line_ind in 1:size(lines, 1))
-    maximum(winners)
+    return nobody
+
 end
 
 
-function get_winner_ind(state_ind)
-    get_winner(display_state(state_ind))
+function get_winner_index(i)
+    get_winner(board_from_index(i))
 end
 
 
-function initial_value(state)
-    winner = get_winner(state)
+function is_board_full(board::Board)
+    all(board.cells .!= [nobody])
+end
 
-    if winner == 2
+
+function initial_value(board::Board)
+    winner = get_winner(board)
+
+    if winner == opponent
         0.0
-    elseif winner == 1
+    elseif winner == me
+        1.0
+    elseif is_board_full(board)
         1.0
     else
         0.5
@@ -60,84 +76,99 @@ end
 
 
 function initial_values()
-    [initial_value(display_state(i)) for i in 1:3^9]
+    [initial_value(board_from_index(i)) for i in 1:3^9]
 end
 
 
-function get_legal_moves(state, player)
-    empty_indices = [x for x in 1:9 if state[x] == 0]
-
-    legal_moves = []
-    for ind in empty_indices
-        next_state = copy(state)
-        next_state[ind] = player
-        push!(legal_moves, next_state)
+function get_legal_moves(board::Board, player::Player)
+    function put_down_player(i::Int64)
+        new_board = Board(board.cells)
+        new_board.cells[i] = player
+        return new_board
     end
 
-    legal_moves
+    [put_down_player(i) for (i, current) in enumerate(board.cells) if current == nobody]
 end
 
 
-function compute_index(state)
+function index_from_board(board::Board)::Int64
     state_ind = 0
     for i in 1:9
-        state_ind += state[i] * 3 ^ (9-i)
+        state_ind += Int64(board.cells[i]) * 3 ^ (9-i)
     end
     state_ind + 1
 end
 
-@assert compute_index(display_state(5)) == 5
 
+@assert index_from_board(board_from_index(5)) == 5
 
-function get_legal_moves_ind(state_ind, player)
-    state = display_state(state_ind)
-    legal_moves = get_legal_moves(state, player)
-    [compute_index(next_state) for next_state in legal_moves]
+#
+# function get_legal_moves_index(i::Int64, player::Player)
+#     board = board_from_index(i)
+#     legal_moves = get_legal_moves(board, player)
+#     [index_from_board(next_state) for next_state in legal_moves]
+# end
+#
+
+function random_move(b::Board, player::Player)
+    rand(get_legal_moves(b, player))
 end
 
-function random_move(state_ind, player)
-    legal_moves = get_legal_moves_ind(state_ind, player)
-    if length(legal_moves) == 0
-        return -1   # sign for 'finished'
-    end
-    rand(legal_moves)
+
+"""
+All the data for a learned model:
+
+- the values
+"""
+struct Model
+    values::Array{Float64, 1}
+    Model() = new(initial_values())
 end
 
 
-function make_move(state_ind, current_values, player)
-    next_moves = get_legal_moves_ind(state_ind, player)
-    if length(next_moves) == 0
-        return -1
-    end
-    next_values = [current_values[next_move] for next_move in next_moves]
+function make_move(model::Model, board::Board, player::Player)
+    next_moves = get_legal_moves(board, player)
+    next_values = [model.values[index_from_board(next_move)] for next_move in next_moves]
     _, max_index = findmax(next_values)
     next_moves[max_index]
 end
 
 
-function pprint_state(state_ind)
-    state = reshape(display_state(state_ind), (3, 3))
-    for i in 1:size(state, 1)
-        println(state[:,i])
+function pprint_state(board::Board)
+    # board = board_from_index(i)
+    board_reshaped = reshape(board.cells, (3,3))
+    for i in 1:3
+        for j in 1:3
+            player = board_reshaped[i, j]
+            if player == me
+                print('X')
+            elseif player == opponent
+                print('O')
+            else
+                print('.')
+            end
+        end
+        print('\n')
     end
+
+    println("\n--------------\n")
 end
 
 
-
-function find_completing(state, line)
+function find_completing(board::Board, line::Array{Int64, 1})
     completing = 0
-    one_counts = 0
-    empty_counts = 0
-    for ind in line
-        if state[ind] == 1
-            one_counts += 1
-        elseif state[ind] == 0
-            completing = ind
-            empty_counts += 1
+    me_counts = 0
+    nobody_counts = 0
+    for i in line
+        if board.cells[i] == me
+            me_counts += 1
+        elseif board.cells[i] == nobody
+            completing = i
+            nobody_counts += 1
         end
     end
 
-    if one_counts == 2 && empty_counts == 1
+    if me_counts == 2 && nobody_counts == 1
         return completing
     else
         return 0
@@ -145,109 +176,70 @@ function find_completing(state, line)
 end
 
 
-function slightly_clever_teacher_opponent(state_ind)
-    state = display_state(state_ind)
-    lines = get_lines()
-
-    for line_ind in 1:size(lines, 1)
-        line = lines[line_ind,:]
-        completing = find_completing(state, line)
+function slightly_clever_teacher_opponent(board::Board)
+    for i in 1:size(LINES, 1)
+        line = LINES[i, :]
+        completing = find_completing(board, line)
         if completing > 0
-            next_state = copy(state)
-            next_state[completing] = 2
-            return compute_index(next_state)
+            next_state = Board(board.cells)
+            next_state.cells[completing] = opponent
+            return next_state
         end
     end
 
-    random_move(state_ind, 2)
+    random_move(board, opponent)
 end
 
 
-function update_values!(values, alpha, exploiting_moves)
-    #print("updating for this game: ")
-    for state_pairs in exploiting_moves
-        state_ind, next_state_ind = state_pairs
-        # print(values[state_ind])
-        # print(" -> ")
-        values[state_ind] = values[state_ind] + alpha * (values[next_state_ind] - values[state_ind])
-        # print(values[state_ind])
-        # print(", ")
-
-        if (state_ind, next_state_ind) == (6861, 8319)
-            println("OK!!")
-        end
+function update_values!(model::Model, alpha::Float64, exploiting_moves::Array{Int64, 1})
+    for i in 2:length(exploiting_moves)
+        a = exploiting_moves[i-1]
+        b = exploiting_moves[i]
+        model.values[a] = model.values[a] + alpha * (model.values[b] - model.values[a])
     end
 end
 
-
-function play_game!(current_values, alpha)
-    state_ind = random_move(1, 2)  # starting state, make a random opponent move on the empty board
-    me = 1
-    opponent = 2
-    move_num = 1
-
-    exploiting_moves = []
-
-    # want to update moving between states that I'm in as a player
-    # I need to choose if I want to optimise for an 'opponent' player or an initial player
-
-    # possible situations:
-    # the game finishes prematurely: no learning
-    # the game finishes normally: perform the updates
-    # but over the correct pairs!
-
-    while true
-        exploring = rand(1:10) == 1
-        if !exploring
-            next_state_ind = make_move(state_ind, current_values, me)
-
-            if next_state_ind == -1
-                exploiting_moves = []
-                break
-            elseif get_winner_ind(next_state_ind) > 0
-                push!(exploiting_moves, (state_ind, next_state_ind))
-                break
-            end
-        else
-            next_state_ind = random_move(state_ind, me)
-            if next_state_ind == -1
-                exploiting_moves = []
-                break
-            elseif get_winner_ind(next_state_ind) > 0
-                push!(exploiting_moves, (state_ind, next_state_ind))
-                break
-            end
-        end
-
-        next_state_ind = slightly_clever_teacher_opponent(next_state_ind)
-        push!(exploiting_moves, (state_ind, next_state_ind))
-        if next_state_ind == -1
-            exploiting_moves = []
-            break
-        elseif get_winner_ind(next_state_ind) > 0
-            break
-        end
-
-        state_ind = next_state_ind
-    end
-
-    if length(exploiting_moves) > 0
-        update_values!(current_values, alpha, exploiting_moves)
-    end
-
-end
 
 # todo: return value difference? For convergence test.
+function train_game!(model::Model, alpha::Float64)
+    exploiting_moves::Array{Int64, 1} = []
+    board = Board()
+    current_player = opponent
+    while true
+        winner = get_winner(board)
+        if winner != nobody
+            break
+        end
+        if is_board_full(board)
+            break
+        end
 
-
-function train(alpha, num_games)
-    srand(345)
-    values = initial_values()
-    for i in 1:num_games
-        play_game!(values, alpha)
+        if current_player == opponent
+            board = slightly_clever_teacher_opponent(board)
+            current_player = me
+        else
+            board = make_move(model, board, current_player)
+            current_player = opponent
+            push!(exploiting_moves, index_from_board(board))
+        end
     end
 
-    values
+    last_index = index_from_board(board)
+    if exploiting_moves[end] != last_index
+        push!(exploiting_moves, last_index)
+    end
+
+    update_values!(model, alpha, exploiting_moves)
+
+end
+
+
+
+function train!(model::Model, alpha::Float64, num_games::Int64)
+    # srand(345)  # random seed
+    for i in 1:num_games
+        train_game!(model, alpha)
+    end
 end
 
 
@@ -305,4 +297,67 @@ function play(values)
 
 end
 
+
+function play_game(model::Model)
+    board = Board()
+    current_player = opponent
+    while true
+        winner = get_winner(board)
+        if winner != nobody
+            return winner
+        end
+        if is_board_full(board)
+            return nobody
+        end
+
+        if current_player == opponent
+            board = slightly_clever_teacher_opponent(board)
+            current_player = me
+        else
+            board = make_move(model, board, current_player)
+            current_player = opponent
+        end
+    end
 end
+
+
+function success_rate(model::Model, num_games::Int64)
+
+    num_successes = 0
+    for i in 1:num_games
+        winner = play_game(model)
+        if winner == me || winner == nobody
+            num_successes += 1
+        end
+    end
+
+    return num_successes / num_games
+end
+
+end  # module
+
+
+"""
+- main function: computes a success metric
+- trains once over 1000 games & then plays another 1000 games and outputs the success rate
+"""
+function main()
+    model = TicTacToe.Model()
+    TicTacToe.train!(model, 0.1, 100000)
+    println("Training done")
+    print(TicTacToe.success_rate(model, 1000))
+
+    # println("HIghest ranking board states: ")
+    # values_enumerated = [x for x in enumerate(model.values)]
+    # values_enumerated = sort(values_enumerated, by=x->x[2])
+    # for pair in values_enumerated
+    #     if pair[2] == 1.0
+    #         break
+    #     end
+    #     println(pair)
+    #     TicTacToe.pprint_state(TicTacToe.board_from_index(pair[1]))
+    #     println("\n-------\n")
+    # end
+end
+
+main()
